701cb84b2d7405d4904c2fca3f14c171e43692ca
박소영학생인턴 <sysy0429@daou.co.kr>
2022-08-16 10:41:41 +0900

    refactor: code 정리

--- src/main/java/com/gwd/tracetool/domain/statistic/event/ErrorStatistic.java
+++ src/main/java/com/gwd/tracetool/domain/statistic/event/ErrorStatistic.java
@@ -8,7 +8,6 @@ import java.util.List;
 public class ErrorStatistic {
 
     private final List<EventNameNode> errorNameNodes = new ArrayList<EventNameNode>();
-    //private final List<EventNameNode> failEventNameNodes = new ArrayList<EventNameNode>();
 
     public void increaseErrorNameStat(String errorName){
         for (EventNameNode errorNameNode : errorNameNodes){
@@ -21,16 +20,5 @@ public class ErrorStatistic {
         errorNode.increaseCount();
         errorNameNodes.add(errorNode);
     }
-//    public void increaseFailEventStat(String failEventName){
-//        for (EventNameNode failEvenNode : failEventNameNodes){
-//            if(failEvenNode.getEventName().equals(failEventName)){
-//                failEvenNode.increaseCount();
-//                return;
-//            }
-//        }
-//        EventNameNode failNode = new EventNameNode(failEventName);
-//        failNode.increaseCount();
-//        failEventNameNodes.add(failNode);
-//    }
 
 }
